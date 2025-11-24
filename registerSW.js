56058e91fc5d23ddca372a8591cf7f8f76d41aeb
Shubhankarmaity <indiavivo956@gmail.com>
2025-11-24 20:22:19 +0000

    Deploying to gh-pages from @ Shubhankarmaity/Tic-Tac-Toe-game@bef2dc1e89cb8eb68dff9ed112dc61b8dc3767a9 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Tic-Tac-Toe-game/sw.js', { scope: '/Tic-Tac-Toe-game/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
